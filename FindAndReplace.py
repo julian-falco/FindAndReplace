20f12061aa53e0f927edb0840d851885b4822d2d
Julian Falco <70605316+julian-falco@users.noreply.github.com>
2021-09-05 17:27:20 -0500

    Better interfacing

--- FindAndReplace.py
+++ FindAndReplace.py
@@ -1,40 +1,64 @@
 import os
+from tkinter import *
+from tkinter.filedialog import *
 
-def getSeriesNameAndNum(dirPath):
-    """Return the series name and section number in a given folder"""
-    
-    os.chdir(dirPath)
-    # find the series file
-    for file in os.listdir("."):
-        if file.endswith(".ser"):
-            seriesName = str(file).replace(".ser", "")
+def getSeriesInfo(fileName):
+    """Return the series name and number of sections."""
+
+    # get only the name
+    seriesName = fileName.replace(".ser", "")
     
     # find out how many sections there are
-    sectionNum = 0
-    while os.path.isfile(seriesName + "." + str(sectionNum)):
-        sectionNum += 1
-        
-    return seriesName, sectionNum
+    sectionNums = []
+    
+    # search each file in the folder that ends with a number
+    for file in os.listdir():
+        try:
+            sectionNums.append(int(file[file.rfind(".")+1:]))
+        except:
+            pass
+
+    # sort the section numbers so they are in order
+    sectionNums.sort()
+
+    return seriesName, sectionNums
 
 try:
-    # gather inputs
-    filePath = input("What is the file path for the folder containing the series?: ")
-    seriesName, sectNum = getSeriesNameAndNum(filePath)
+    input("Press enter to open your file explorer and select the series file.")
+
+    # create tkinter object but don't display extra window
+    root = Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+
+    # open file explorer to series file
+    fileName = askopenfilename(title="Open a Series File",
+                           filetypes=(("Series File", "*.ser"),
+                                      ("All Files","*.*")))
+    if not fileName:
+        raise Exception("No file selected.")
+
+    # get needed series info
+    filePath = fileName[:fileName.rfind("/")]
+    seriesFile = fileName[fileName.rfind("/"):]
+    os.chdir(filePath)
+    
+    seriesName, sectNums = getSeriesInfo(seriesFile)
 
     # warn user that the trace files will be rewritten
     input("\nWARNING: This program will edit the trace files and cannot be undone.\n" +
-          "Backing up your trace files is recommended.\n" +
-          "Press enter to continue or Ctrl+c at any time to exit.\n")
+          "Backing up your trace files is recommended." +
+          "\nPress enter to continue.")
 
     # ask for what should be searched for and replaced
-    find = input("Enter the set of characters you would like to search for: ")
+    find = input("\nEnter the set of characters you would like to search for: ")
     replace = input("Enter the set of characters you would like to replace it with: ")
 
     # check if user wants to only replace certain instances of the character set
-    print("If you wish to replace all instances of " + find + " with " + replace + ", then enter the number 1 below.")
+    print("\nIf you wish to replace all instances of " + find + " with " + replace + ", then enter the number 1 below.")
     print("If you would like to specifically choose what to replace, then enter the number 2 below.")
 
-    replaceAll = input("What would you like to do?: ")
+    replaceAll = input("\nWhat would you like to do? (enter 1 or 2): ")
     while replaceAll != "1" and replaceAll != "2":
         replaceAll = input("Please enter a valid answer. (y/n): ")
     print()
@@ -42,10 +66,10 @@ try:
     # find everything that matches what is being searched for and print it for the user
     # allow user to select which ones to replace if desired
     toReplace = []
-    for i in range(sectNum):
+    for i in sectNums:
 
         # read the file into a list
-        fileName = filePath + "\\" + seriesName + "." + str(i)
+        fileName = filePath + "/" + seriesName + "." + str(i)
         sectionFile = open(fileName, "r")
         lines = sectionFile.readlines()
         sectionFile.close()
@@ -89,7 +113,8 @@ try:
                 toReplace.append(splitLine[1])
     
     # final confirmation
-    input("\nPress enter to replace all selected instances.")
+    input("\nPress enter to confirm replacement of all selected instances.\n" +
+          "This CANNOT be undone.")
 
     # iterate through files, doing replacements
     for i in range(sectNum):
